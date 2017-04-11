e905d9a3306cc39958bfae92a8b185fbacb7ab22
Triana Hamilton <mavb654@live.rhul.ac.uk>
2017-04-11 10:39:08 +0100

    Added sponsors to home page.

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var logoList = require('./model/logo')
+var sponsorList = require('./model/sponsor')
 var doc = require('./utils/doc')
 var quotesInspiration = require('./model/quotesInspiration')
 var quotesCrockford = require('./model/quotesCrockford')
@@ -11,6 +12,7 @@ router.get('/', function (req, res) {
     title: 'Welcome to JavaScript 101',
     message: 'We are a JavaScript meetup group in Bristol, UK.',
     logoList: logoList,
+    sponsorList: sponsorList,
     quotesInspiration: quotesInspiration,
     quotesCrockford: quotesCrockford,
     quotesJokes: quotesJokes
