5b2245f5bb7acfe6f6d0f90f6a94efb7776017e9
Dave Thomson <djthomson@gmail.com>
2017-05-11 00:39:53 +0100

    Divide logos into groups

---
+++ model/meetup.js
@@ -0,0 +1,16 @@
+var logoList = [
+  {
+    title: "CodeHub",
+    url: "https://www.meetup.com/CodeHub-Bristol/",
+    src: "/img/logo/codehub.png",
+    alt: "CodeHub is a place to code, share, and learn from one another. If you are developing for the web and wish to exchange knowledge and skills, come and join us. We are largely based within a 25 mile radius of Bristol/Bath, but anyone welcome."
+  },
+  {
+    title: "Bristol JS",
+    url: "https://www.meetup.com/BristolJS/",
+    src: "/img/logo/bristol-js.png",
+    alt: "Bristol JS"
+  }
+];
+
+module.exports = logoList
---
+++ model/tech.js
@@ -0,0 +1,40 @@
+var logoList = [
+  {
+    title: "JavaScript",
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/",
+    src: "/img/logo/js.png",
+    alt: "JavaScript"
+  },
+  {
+    title: "Node JS",
+    url: "https://nodejs.org/",
+    src: "/img/logo/nodejs.png",
+    alt: "Node JS"
+  },
+  {
+    title: "NPM",
+    url: "https://www.npmjs.com/",
+    src: "/img/logo/npm.png",
+    alt: "NPM"
+  },
+  {
+    title: "React",
+    url: "https://facebook.github.io/react/",
+    src: "/img/logo/react.png",
+    alt: "React"
+  },
+  {
+    title: "Github",
+    url: "https://github.com/CodeHubOrg",
+    src: "/img/logo/github.png",
+    alt: "Github"
+  },
+  {
+    title: "Ubuntu",
+    url: "https://www.ubuntu.com/",
+    src: "/img/logo/ubuntu.png",
+    alt: "Ubuntu"
+  }
+];
+
+module.exports = logoList
